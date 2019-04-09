7bdf8191e81bc0afcfe9c824d96fa5271ae0bfd6
Alvaro <alvaro.serrano@wmich.edu>
2019-04-09 17:08:51 -0400

    Started working on calculateResult in PostFixExpression

--- src/edu/wmich/cs1120/LA6_S19/s544_4_LA6/PostFixExpression.java
+++ src/edu/wmich/cs1120/LA6_S19/s544_4_LA6/PostFixExpression.java
@@ -13,7 +13,24 @@ public class PostFixExpression implements IPostfixExpression {
 	 */
 	@Override
 	public int calculateResult() {
-	
+		//Loop over expression and push every char to the stack
+		//Create a temporary array
+		String temp[] = new String[expression.length()];
+		temp = expression.split(",");
+		String tempString = "";
+		for (int i = 0; i < temp.length; i++) {
+			//Push onto the stack
+			//Trim spaces
+			tempString = temp[i];
+			int right = 0;
+			int left = 0;
+			switch(tempString) {
+			
+			default:
+				stack.push(Integer.parseInt(tempString));
+				break;
+			}//switch
+		}//for
 		return 0;
 	}
 
